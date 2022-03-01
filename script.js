e1e754b3207ba00223694e51a765b82653ff1d46
paltoalko <martas90909@me.com>
2022-03-02 00:09:31 +0100

    almost done wihout js

--- script.js
+++ script.js
@@ -1,24 +1,106 @@
-const playbtn = document.querySelector(".playbtn")
+function game () {
+  const X_CLASS = 'x'
+  const CIRCLE_CLASS = 'circle'
+  const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const restartButton = document.querySelector(".restart")
 
-function hidestartscreen () {
-    document.querySelector(".startscreen").style.display = "none";
-    document.querySelector(".gamescreen").style.display = "flex";
-    document.querySelector(".bottom-gamescreen").style.display="none";
-    fixPadding();
+
+let circleTurn
+
+startGame()
+
+restartButton.addEventListener('click', startGame)
+
+function startGame() {
+  circleTurn = false
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(CIRCLE_CLASS)
+    cell.removeEventListener('click', handleClick)
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
 }
 
-function fixPadding () {
-    const gamemodes = document.querySelector(".bottom-gamescreen")
-    const gamescreen = document.querySelector(".maingamescreen")
-
-    if (gamemodes.style.display == "none") {
-        gamescreen.style.padding = "32px 16px";
-    } 
+function handleClick(e) {
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  placeMark(cell, currentClass)
+  if (checkWin(currentClass)) {
+    endGame(false)
+  } else if (isDraw()) {
+    endGame(true)
+  } else {
+    swapTurns()
+    setBoardHoverClass()
+  }
 }
 
-function startGame () {
-    playbtn.addEventListener("click", () => {hidestartscreen();})
-
+function endGame(draw) {
+  if (draw) {
+    console.log("draw");
+    
+  } else {
+    console.log("wins");
+    
+  }
 }
 
-startGame();
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+  })
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+  circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS)
+  board.classList.remove(CIRCLE_CLASS)
+  if (circleTurn) {
+    board.classList.add(CIRCLE_CLASS)
+  } else {
+    board.classList.add(X_CLASS)
+  }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
+}
+
+function players() {
+const playeravatar = document.querySelectorAll(".icons")
+playeravatar.forEach(player => {player.addEventListener("click", () => {
+  console.log("dupa");
+  
+})})
+
+// create player with name and avatar 
+// set 
+
+}
+players();
+}
+
+game();
