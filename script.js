1d0d7a9f0ca8fd6389e466e19664a879e791d97a
paltoalko <martas90909@me.com>
2022-03-02 22:21:17 +0100

    finished

--- script.js
+++ script.js
@@ -14,13 +14,18 @@ function game () {
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
 const restartButton = document.querySelector(".restart")
-
-
+const startButton = document.querySelector(".startbtn")
+const playeronepoints = document.querySelector(".playeronescore")
+const playertwopoints = document.querySelector(".playertwoscore")
+const roundcounter = document.querySelector(".roundcounter")
+let onepoints = 0;
+let twopoints = 0;
+let round = 1;
+playeronepoints.innerHTML = onepoints
+playertwopoints.innerHTML = twopoints
+roundcounter.innerHTML = `Round ${round}`
 let circleTurn
 
-startGame()
-
-restartButton.addEventListener('click', startGame)
 
 function startGame() {
   circleTurn = false
@@ -50,12 +55,36 @@ function handleClick(e) {
 function endGame(draw) {
   if (draw) {
     console.log("draw");
+    round++
+    roundcounter.innerHTML = `Round ${round}`
+    clearBoard();
     
   } else {
-    console.log("wins");
+   if (!circleTurn){
+     onepoints++
+     round++
+     playeronepoints.innerHTML = onepoints
+     roundcounter.innerHTML = `Round ${round}`
+     console.log("x wins");
+     
+   } else if (circleTurn) {
+    twopoints++
+    round++
+    playertwopoints.innerHTML = twopoints
+    roundcounter.innerHTML = `Round ${round}`
+    console.log("o wins");
+   }
+   clearBoard()
     
   }
 }
+
+
+function clearBoard () {
+  document.querySelectorAll(".cell").forEach(cell => cell.classList.remove("x","circle"))
+  startGame()
+}
+
 
 function isDraw() {
   return [...cellElements].every(cell => {
@@ -91,15 +120,108 @@ function checkWin(currentClass) {
 
 function players() {
 const playeravatar = document.querySelectorAll(".icons")
-playeravatar.forEach(player => {player.addEventListener("click", () => {
-  console.log("dupa");
-  
-})})
+const playeroneavatar = document.getElementById("playerone")
+const playertwoavatar = document.getElementById("playertwo") 
 
-// create player with name and avatar 
-// set 
+playeroneavatar.classList.add("fa-solid", "fa-user", "fa-3x")
+playertwoavatar.classList.add("fa-solid", "fa-user-astronaut", "fa-3x")
+
+
+// set players avatar
+function changeAvatar () {
+  const oneuser = document.getElementById("oneuser")
+  const oneastronaut = document.getElementById("oneastronaut")
+  const oneninja = document.getElementById("oneninja")
+  const onetie = document.getElementById("onetie")
+  const twouser = document.getElementById("twouser")
+  const twoastronaut = document.getElementById("twoastronaut")
+  const twoninja = document.getElementById("twoninja")
+  const twotie = document.getElementById("twotie")
+
+  oneuser.setAttribute("id", "selected")
+  twoastronaut.setAttribute("id", "selected")
+
+  oneuser.addEventListener("click", () => {
+    oneastronaut.removeAttribute("id","selected")
+    oneninja.removeAttribute("id","selected")
+    onetie.removeAttribute("id","selected")
+    playeroneavatar.removeAttribute("class")
+    playeroneavatar.classList.add("fa-solid", "fa-user", "fa-3x")
+    oneuser.setAttribute("id", "selected")
+  })
+  oneastronaut.addEventListener("click", () => {
+    oneuser.removeAttribute("id","selected")
+    oneninja.removeAttribute("id","selected")
+    onetie.removeAttribute("id","selected")
+    playeroneavatar.classList.add("fa-solid", "fa-user-astronaut", "fa-3x")
+    oneastronaut.setAttribute("id", "selected")
+  })
+  oneninja.addEventListener("click", () => {
+    oneastronaut.removeAttribute("id","selected")
+    onetie.removeAttribute("id","selected")
+    oneuser.removeAttribute("id","selected")
+    playeroneavatar.removeAttribute("class")
+    playeroneavatar.classList.add("fa-solid", "fa-user-ninja", "fa-3x")
+    oneninja.setAttribute("id", "selected")
+  })
+  onetie.addEventListener("click", () => {
+    oneastronaut.removeAttribute("id","selected")
+    oneninja.removeAttribute("id","selected")
+    oneuser.removeAttribute("id","selected")
+    playeroneavatar.removeAttribute("class")
+    playeroneavatar.classList.add("fa-solid", "fa-user-tie", "fa-3x")
+    onetie.setAttribute("id", "selected")
+  })
+  twouser.addEventListener("click", () => {
+    twoastronaut.removeAttribute("id","selected")
+    twoninja.removeAttribute("id","selected")
+    twotie.removeAttribute("id","selected")
+    playertwoavatar.removeAttribute("class")
+    playertwoavatar.classList.add("fa-solid", "fa-user", "fa-3x")
+    twouser.setAttribute("id", "selected")
+  })
+  twoastronaut.addEventListener("click", () => {
+    twouser.removeAttribute("id","selected")
+    twoninja.removeAttribute("id","selected")
+    twotie.removeAttribute("id","selected")
+    playertwoavatar.removeAttribute("class")
+    playertwoavatar.classList.add("fa-solid", "fa-user-astronaut", "fa-3x")
+    twoastronaut.setAttribute("id", "selected")
+  })
+  twoninja.addEventListener("click", () => {
+    twoastronaut.removeAttribute("id","selected")
+    twotie.removeAttribute("id","selected")
+    twouser.removeAttribute("id","selected")
+    playertwoavatar.removeAttribute("class")
+    playertwoavatar.classList.add("fa-solid", "fa-user-ninja", "fa-3x")
+    twoninja.setAttribute("id", "selected")
+  })
+  twotie.addEventListener("click", () => {
+    twoastronaut.removeAttribute("id","selected")
+    twoninja.removeAttribute("id","selected")
+    twouser.removeAttribute("id","selected")
+    playertwoavatar.removeAttribute("class")
+    playertwoavatar.classList.add("fa-solid", "fa-user-tie", "fa-3x")
+    twotie.setAttribute("id", "selected")
+  })
+
+
 
 }
+changeAvatar();
+
+}
+
+startButton.addEventListener("click", () => {
+  document.querySelector(".popup").style.display = "none"
+  startGame()
+})
+
+restartButton.addEventListener('click', () => {
+  location.reload();
+})
+
+
 players();
 }
 
